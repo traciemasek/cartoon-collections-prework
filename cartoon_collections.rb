9fa1380574406f08e1161bedd3479b5d06130a1c
Tracie Masek <traciemasek@gmail.com>
2019-07-17 10:41:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.any? { |call| call.}
+  calls.any? { |call| call.length > 4}
 end
 
 def find_the_cheese# code an argument here
