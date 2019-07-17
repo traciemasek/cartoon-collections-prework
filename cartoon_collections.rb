5180f08103c4bea73ce3e72d33f0c7422856a708
Tracie Masek <traciemasek@gmail.com>
2019-07-17 10:38:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(calls)
-  # Your code here
+  calls.collect { |call| "#{call.capitalize}!"}
 end
 
 def long_planeteer_calls# code an argument here
